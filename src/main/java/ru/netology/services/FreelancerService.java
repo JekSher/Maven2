6b4975d20070bee32616389b8ba64870df5c124d
Sher <jeksher.media@gmail.com>
2023-11-13 15:22:02 +0300

    add src/main/java/ru/netology/services/FreelancerService.java

--- src/main/java/ru/netology/services/FreelancerService.java
+++ src/main/java/ru/netology/services/FreelancerService.java
@@ -1,3 +1,23 @@
-package ru.netology.services
+package ru.netology.services;
+
 public class FreelancerService {
+    int calculate(int income, int expenses, int threshold) {
+        int balance = 0;
+        int counter = 0;
+
+        for (int i = 0; i < 12; i++) {
+            if (balance < threshold) {
+                //       работаем
+                balance += income;
+                balance -= expenses;
+            } else {
+                //       отдыхаем
+                balance -= expenses;
+                balance /= 3;
+                counter++;
+            }
+        }
+
+        return counter;
+    }
 }
